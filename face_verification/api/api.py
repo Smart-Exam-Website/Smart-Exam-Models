16f593bc50c728a70230c4122363c05a3a5ac575
nermeen_atef <34457317+nermeenatif@users.noreply.github.com>
2022-02-15 23:01:05 +0200

    edit

--- face_verification/api/api.py
+++ face_verification/api/api.py
@@ -74,7 +74,7 @@ def verify():
     
 	#return str(resp_obj['pair_1']['verified']), 200
 	#return resp_obj, 200
-	return True, 200
+	return {"verified":resp_obj['pair_1']['verified']},200
 
 def verifyWrapper(req, trx_id = 0):
 
