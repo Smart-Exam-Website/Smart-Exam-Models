2b0c0c17c737cf314d6e6e74d0e8c29aee87c36f
Ubuntu <ubuntu@ip-172-31-25-62.us-east-2.compute.internal>
2022-03-15 05:46:47 +0000

    clean folders

--- face_detection/face_detection/detector.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/Smart-Exam-Models/face_detection')
-
-from api import app as application
--- face_detection/face_detection/run.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/detect")
-def hello():
-    return "Hello, detector!"
-if __name__ == "__main__":
-    app.run()
