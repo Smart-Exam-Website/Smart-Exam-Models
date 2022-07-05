b8c8e7e732e20a2c32fa3513d64728bc66a01dea
Heba Atef <hoba30.ha@gmail.com>
2022-07-05 23:08:42 +0200

    fixing the response image colors & encoding

--- face_detection/api.py
+++ face_detection/api.py
@@ -23,18 +23,12 @@ class Face_Detector():
         self.face_detector = FaceDetector(self.MODEL_PATH, gpu_memory_fraction=0.25, visible_device_list='0')
     
     def decode_image(self, image_b64encode):
-        # if len(img2) > 11 and img2[0:11] == "data:image/":
-		# 		validate_img2 = True
-
         _, _, image_b64encode = image_b64encode.partition(',')
         im_bytes = base64.b64decode(image_b64encode)
-        # im_bytes = base64.decodestring(image_b64encode)
 
         im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
         img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # image = Image.fromarray(img)
-        # image.show()
         return img
 
     def encode_image(self, image):
@@ -42,7 +36,6 @@ class Face_Detector():
         _, im_arr = cv2.imencode('.jpg', image)  # im_arr: image in Numpy one-dim array format.
         im_bytes = im_arr.tobytes()
         image_encode = base64.b64encode(im_bytes)
-        # image_encode = base64.encodestring(im_bytes)        
         return image_encode
 
     def draw_boxes_on_image(self, image, boxes, scores):
@@ -66,8 +59,9 @@ class Face_Detector():
 
     def detect(self, image_array):
         boxes, scores = self.face_detector(image_array, score_threshold=0.3)
-        annotated_image, number_of_faces = self.draw_boxes_on_image(Image.fromarray(image_array), boxes, scores)
-        # annotated_image.show()
+        img = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+
+        annotated_image, number_of_faces = self.draw_boxes_on_image(Image.fromarray(img), boxes, scores)
         return annotated_image, number_of_faces
 
     def run(self, img_encode):
@@ -76,6 +70,7 @@ class Face_Detector():
         annotated_image_encoded = ""
         if number_of_faces > 1:
             annotated_image_encoded = self.encode_image(annotated_image)
+            annotated_image_encoded = b'data:image/jpeg;base64,' + annotated_image_encoded
         return annotated_image_encoded, number_of_faces
 
 ########################################################################3
@@ -98,7 +93,6 @@ class Face_Detector_API(Resource):
 api.add_resource(Face_Detector_API, "/detect")
 
 if __name__ == "__main__":
-    # app.run(debug = False) 
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
 		'-p', '--port',
@@ -107,41 +101,3 @@ if __name__ == "__main__":
 		help='Port of serving api')
 	args = parser.parse_args()
 	app.run(host='0.0.0.0', port=args.port)
-
-    # with open("correct_result.txt") as f:
-    #     text = f.read()
-
-    # fd = Face_Detector()
-    # annotated_image_encoded, number_of_faces = fd.run(text)
-    # fd.decode_image(annotated_image_encoded)
-
-
-    # fd = Face_Detector()
-    # di = Detection_Interface()
-    # path ="images/3.jpg"
-
-
-    # img = di.decode_image(text)
-    # annotated_image, number_of_faces = fd.run(img)
-
-    # img = di.encode_image(annotated_image)
-    # # output = io.BytesIO()
-    # with open('encode_ann_result.txt', 'w') as f:
-    #     f.write(str(img))
-
-    # img = di.decode_image(img)
-
-    # image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # image = Image.fromarray(image)
-    # # image.save("ff.jpg")
-    # image.show()
-
-    # f1 = "encode_ann_result.txt"
-    # f2 = "correct_ann_result.txt"
-    
-    # # shallow comparison
-    # result = filecmp.cmp(f1, f2)
-    # print(result)
-    # # deep comparison
-    # result = filecmp.cmp(f1, f2, shallow=False)
-    # print(result)
